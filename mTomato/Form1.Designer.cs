9d56a477604a64c9a0f0632ef46222f41a87da16
Milkey Tan <819774989@qq.com>
2018-11-05 11:05:40 +0800

    Tomato

---
+++ mTomato/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace mTomato
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnStart = new MaterialSkin.Controls.MaterialFlatButton();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.timerRset = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuItemExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnStop = new MaterialSkin.Controls.MaterialFlatButton();
+            this.ProgressBarTime = new MaterialSkin.Controls.MaterialProgressBar();
+            this.timerShow = new System.Windows.Forms.Timer(this.components);
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuItemStop = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemStart = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuItemAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.AutoSize = true;
+            this.btnStart.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnStart.Depth = 0;
+            this.btnStart.Icon = null;
+            this.btnStart.Location = new System.Drawing.Point(10, 81);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.btnStart.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Primary = false;
+            this.btnStart.Size = new System.Drawing.Size(64, 36);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1500000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // timerRset
+            // 
+            this.timerRset.Interval = 300000;
+            this.timerRset.Tick += new System.EventHandler(this.timerRset_Tick);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon.Text = "mTomato - 番茄！";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuItemStop,
+            this.menuItemStart,
+            this.toolStripSeparator1,
+            this.menuItemAbout,
+            this.toolStripSeparator2,
+            this.menuItemExit});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(101, 104);
+            // 
+            // menuItemExit
+            // 
+            this.menuItemExit.Name = "menuItemExit";
+            this.menuItemExit.Size = new System.Drawing.Size(100, 22);
+            this.menuItemExit.Text = "退出";
+            this.menuItemExit.Click += new System.EventHandler(this.menuItemExit_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.AutoSize = true;
+            this.btnStop.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnStop.Depth = 0;
+            this.btnStop.Icon = null;
+            this.btnStop.Location = new System.Drawing.Point(173, 82);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.btnStop.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Primary = false;
+            this.btnStop.Size = new System.Drawing.Size(56, 36);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // ProgressBarTime
+            // 
+            this.ProgressBarTime.Depth = 0;
+            this.ProgressBarTime.Location = new System.Drawing.Point(9, 70);
+            this.ProgressBarTime.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ProgressBarTime.MouseState = MaterialSkin.MouseState.HOVER;
+            this.ProgressBarTime.Name = "ProgressBarTime";
+            this.ProgressBarTime.Size = new System.Drawing.Size(220, 5);
+            this.ProgressBarTime.TabIndex = 3;
+            // 
+            // timerShow
+            // 
+            this.timerShow.Interval = 1000;
+            this.timerShow.Tick += new System.EventHandler(this.timerShow_Tick);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(97, 6);
+            // 
+            // menuItemStop
+            // 
+            this.menuItemStop.Enabled = false;
+            this.menuItemStop.Name = "menuItemStop";
+            this.menuItemStop.Size = new System.Drawing.Size(100, 22);
+            this.menuItemStop.Text = "停止";
+            this.menuItemStop.Click += new System.EventHandler(this.menuItemStop_Click);
+            // 
+            // menuItemStart
+            // 
+            this.menuItemStart.Name = "menuItemStart";
+            this.menuItemStart.Size = new System.Drawing.Size(100, 22);
+            this.menuItemStart.Text = "开始";
+            this.menuItemStart.Click += new System.EventHandler(this.menuItemStart_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(97, 6);
+            // 
+            // menuItemAbout
+            // 
+            this.menuItemAbout.Name = "menuItemAbout";
+            this.menuItemAbout.Size = new System.Drawing.Size(100, 22);
+            this.menuItemAbout.Text = "关于";
+            this.menuItemAbout.Click += new System.EventHandler(this.menuItemAbout_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(238, 125);
+            this.Controls.Add(this.ProgressBarTime);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "mTomato";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialFlatButton btnStart;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Timer timerRset;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private MaterialSkin.Controls.MaterialFlatButton btnStop;
+        private MaterialSkin.Controls.MaterialProgressBar ProgressBarTime;
+        private System.Windows.Forms.Timer timerShow;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem menuItemExit;
+        private System.Windows.Forms.ToolStripMenuItem menuItemStop;
+        private System.Windows.Forms.ToolStripMenuItem menuItemStart;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem menuItemAbout;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+    }
+}
+
---
+++ mTomato/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using MaterialSkin.Controls;
+using MaterialSkin;
+
+namespace mTomato
+{
+    public partial class Form1 : MaterialForm
+    {
+
+        Icon ico = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            MaximizeBox = false;
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Red800, Primary.Red900, Primary.Green500, Accent.LightBlue200, TextShade.WHITE);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnStop.Enabled = false;
+
+            notifyIcon.Icon = ico;
+            notifyIcon.Visible = true;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = true;
+
+            notifyIcon.ShowBalloonTip(5000, "mTomato - 番茄！", "番茄钟已开始", ToolTipIcon.None);
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            menuItemStart.Enabled = false;
+            menuItemStop.Enabled = true;
+
+            ProgressBarTime.Maximum = 1500000;
+            timerShow.Enabled = true;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+            notifyIcon.ShowBalloonTip(5000, "mTomato - 番茄！", "25分钟工作时间已结束 开始休息吧！", ToolTipIcon.None);
+            timerRset.Enabled = true;
+        }
+
+        private void timerRset_Tick(object sender, EventArgs e)
+        {
+            timerRset.Enabled = true;
+            notifyIcon.ShowBalloonTip(5000, "mTomato - 番茄！", "5分钟休息时间已结束 开始工作吧！", ToolTipIcon.None);
+            timer.Enabled = false;
+
+            timerShow.Enabled = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            notifyIcon.ShowBalloonTip(5000, "mTomato - 番茄！", "番茄钟已停止", ToolTipIcon.None);
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            menuItemStart.Enabled = true;
+            menuItemStop.Enabled = false;
+
+            timeValue = 0;
+            timerRset.Enabled = false;
+            timer.Enabled = false;
+        }
+
+        int timeValue = 1500000;
+        private void timerShow_Tick(object sender, EventArgs e)
+        {
+            if (timeValue == 0)
+            {
+                timeValue = 1500000;
+                timerShow.Enabled = false;
+            }
+
+            timeValue = timeValue - 1000;
+            ProgressBarTime.Value = timeValue;
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                WindowState = FormWindowState.Normal;
+                Activate();
+                ShowInTaskbar = true;
+                notifyIcon.Visible = false;
+            }
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                notifyIcon.Icon = ico;
+                ShowInTaskbar = false;
+                notifyIcon.Visible = true;
+            }
+        }
+
+        private void menuItemExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+            Application.ExitThread();
+        }
+
+        private void menuItemAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("mTomato 1.0.0\n\rCopyright © 2014~2017 Mili Tan. All Rights Reserved. \n\ricon by Aowxtz\n\r感谢使用！");
+        }
+
+        private void menuItemStop_Click(object sender, EventArgs e)
+        {
+            btnStop_Click(null, null);
+        }
+
+        private void menuItemStart_Click(object sender, EventArgs e)
+        {
+            btnStart_Click(null, null);
+        }
+    }
+}

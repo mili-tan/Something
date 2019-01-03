aec1f7cad78fd9c6d9ab92bc8966d1956322f1d0
Milkey Tan <819774989@qq.com>
2019-01-03 13:08:57 +0800

    update

--- ImgCut/ImgCut/Program.cs
+++ ImgCut/ImgCut/Program.cs
@@ -1,36 +1,50 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Drawing.Imaging;
+using System.IO;
 
 namespace ImgCut
 {
     static class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            var sorceB = new Bitmap(args[0]);
-            var b0 = CutImage(sorceB, 0, 0, 452, 1920);
-            var b1 = CutImage(sorceB, 452, 0, 904, 1920);
-            var b2 = CutImage(sorceB, 904, 0, 1356, 1920);
+            foreach (var itemFile in Directory.GetFiles(@"pics", "*.*", SearchOption.AllDirectories))
+            {
+                var sourceB = new Bitmap(itemFile);
+                int wCut = sourceB.Width / 3;
+                int hCut = sourceB.Height / 3;
 
-            Bitmap imgStep1 = new Bitmap(1356, 1920);
-            var g0 = Graphics.FromImage(imgStep1);
-                g0.DrawImage(b1, new Point(0, 0));
-                g0.DrawImage(b0, new Point(452, 0));
-                g0.DrawImage(b2, new Point(904, 0));
-                g0.Save();
-            
+                var b0 = CutImage(sourceB, 0, 0, wCut, sourceB.Height);
+                var b1 = CutImage(sourceB, wCut, 0, wCut * 2, sourceB.Height);
+                var b2 = CutImage(sourceB, wCut * 2, 0, sourceB.Width, sourceB.Height);
 
-            var bS0 = CutImage(imgStep1, 0, 0, 1356, 640);
-            var bS1 = CutImage(imgStep1, 0, 640, 1356, 1920);
+                Bitmap imgStep1 = new Bitmap(sourceB.Width, sourceB.Height);
+                using (var g0 = Graphics.FromImage(imgStep1))
+                {
+                    g0.DrawImage(b1, new Point(0, 0));
+                    g0.DrawImage(b0, new Point(wCut, 0));
+                    g0.DrawImage(b2, new Point(wCut * 2, 0));
+                    g0.Save();
+                }
 
-            Bitmap imgStep2 = new Bitmap(1356, 1920);
-            var g1 = Graphics.FromImage(imgStep2);
-                g1.DrawImage(bS1, new Point(0, 0));
-                g1.DrawImage(bS0, new Point(0, 1280));
-                g1.Save();
-            
 
-            imgStep2.Save("new.png",ImageFormat.Png);
+                var bS0 = CutImage(imgStep1, 0, 0, sourceB.Width, hCut);
+                var bS1 = CutImage(imgStep1, 0, hCut, sourceB.Width, sourceB.Height);
+
+                Bitmap imgStep2 = new Bitmap(sourceB.Width, sourceB.Height);
+                using (var g1 = Graphics.FromImage(imgStep2))
+                {
+                    g1.DrawImage(bS1, new Point(0, 0));
+                    g1.DrawImage(bS0, new Point(0, sourceB.Height - hCut));
+                    g1.Save();
+                }
+
+                Console.WriteLine(itemFile);
+                imgStep2.Save("./newpic/" + itemFile.Replace("pics\\","").Replace(".","-") + ".png", ImageFormat.Png);
+            }
+            Console.WriteLine("Done!");
+            Console.ReadKey();
         }
 
         public static Bitmap CutImage(Bitmap bitmap, int beginX, int beginY, int getX, int getY)

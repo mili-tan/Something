dc9b233f387916c7b2e229e4996dfa79e059c84e
Milkey Tan <819774989@qq.com>
2019-01-16 11:17:53 +0800

    Nancy

---
+++ NancyTest/NancyTest/Modules/HomeModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Nancy;
+
+namespace NancyTest.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = p => "Welcome to Once Data Storage API";
+
+            Get["/get/{gid}"] = p => File.Exists($"./data/{p.gid}/storage.t") ? File.ReadAllLines($"./data/{p.gid}/storage.t")[0] : "not found";
+
+            Get["/set/{gid}/{mContext}"] = p =>
+            {
+                if (!Directory.Exists($"./data/{p.gid}"))
+                {
+                    Directory.CreateDirectory($"./data/{p.gid}");
+                    if (!File.Exists($"./data/{p.gid}/storage.t"))
+                        File.Create($"./data/{p.gid}/storage.t").Close();
+                }
+                File.WriteAllText($"./data/{p.gid}/storage.t",
+                    p.mContext.ToString() + Environment.NewLine + File.ReadAllText($"./data/{p.gid}/storage.t"));
+                return "OK";
+            };
+        }
+    }
+}
---
+++ NancyTest/NancyTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Nancy.Hosting.Self;
+
+namespace NancyTest
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Once Data Storage API");
+
+            using (NancyHost host = new NancyHost(new HostConfiguration
+                    {RewriteLocalhost = true, UrlReservations = new UrlReservations {CreateAutomatically = true}},
+                    new Uri("http://localhost:11108/")))
+            {
+                host.Start();
+
+                Console.WriteLine("Data Storage API is running on 11108 port");
+                Console.WriteLine("Press any [Enter] to close the host.");
+                Console.ReadLine();
+            }
+        }
+    }
+}

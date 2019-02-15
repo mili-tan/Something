f94daa76f88156e7526150ed47cf1c9fa73b7395
Milkey Tan <819774989@qq.com>
2019-02-15 16:38:42 +0800

    ipfs-test

---
+++ ipfs-test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using Ipfs.Http;
+
+namespace IPFSTest
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var ipfs = new IpfsClient("http://127.0.0.1:5002");
+            Console.WriteLine(ipfs.FileSystem.ReadAllTextAsync("Qmf412jQZiuVUtdgnB36FXFX7xg5V6KEbSJ4dpQuhkLyfD").Result);
+            Console.WriteLine(ipfs.FileSystem.AddTextAsync("just test").Result.Id);
+            Console.WriteLine(ipfs.Swarm.PeersAsync().Result.Count());
+            Console.ReadKey();
+        }
+    }
+}

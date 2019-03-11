5b92e50946236041d8b80220f53f1b41e761e854
Milkey Tan <819774989@qq.com>
2019-03-11 15:18:26 +0800

    sth

---
+++ MUPnPTest/OvQ/OvQ-C/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using QuicNet;
+using QuicNet.Connections;
+using QuicNet.Streams;
+
+namespace OvQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+            QuicClient qClient = new QuicClient();
+            QuicConnection qConnection = qClient.Connect("127.0.0.1", 11000);
+            QuicStream qStream = qConnection.CreateStream(QuickNet.Utilities.StreamType.ClientBidirectional);
+
+            Console.ReadKey();
+            qStream.Send(Encoding.UTF8.GetBytes("Hello from Client!"));
+            Console.WriteLine(qStream.Receive().ToString());
+        }
+    }
+}
---
+++ MUPnPTest/OvQ/OvQ-S/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using QuicNet;
+using QuicNet.Connections;
+
+namespace OvQ_S
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuicListener listener = new QuicListener(11000);
+            listener.Start();
+            while (true)
+            {
+
+                QuicConnection client = listener.AcceptQuicClient();
+
+                client.OnDataReceived += (c) => {
+                    byte[] data = c.Data;
+                    Console.WriteLine("Data received: " + Encoding.UTF8.GetString(data));
+                    c.Send(Encoding.UTF8.GetBytes("Echo!"));
+                };
+            }
+        }
+    }
+}

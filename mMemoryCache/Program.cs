001c1048c5df93f151e6ce2e42c43289d3c60801
Milkey Tan <819774989@qq.com>
2019-04-03 10:26:31 +0800

    update

--- mMemoryCache/Program.cs
+++ mMemoryCache/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Runtime.Caching;
 using System.Threading;
 
@@ -11,14 +12,22 @@ namespace mMemoryCache
             var cacheItem = new CacheItem("test", "just for test");
             var cachePolicy = new CacheItemPolicy(){SlidingExpiration = new TimeSpan(0,0,0,10)};
             MemoryCache.Default.Add(cacheItem, cachePolicy);
+            MemoryCache.Default.Add(new CacheItem("bala","balabala"), new CacheItemPolicy(){SlidingExpiration = TimeSpan.Zero});
 
             Console.WriteLine(MemoryCache.Default.Contains("test").ToString());
             if (MemoryCache.Default.Contains("test"))
                 Console.WriteLine(MemoryCache.Default["test"]);
+            Console.WriteLine(MemoryCache.Default.Count());
             
             Thread.Sleep(11000);
             Console.WriteLine("Thread.Sleep(11000)");
             Console.WriteLine(MemoryCache.Default.Contains("test").ToString());
+            Console.WriteLine(MemoryCache.Default.Count());
+
+            MemoryCache.Default.Dispose();
+            Console.WriteLine("MemoryCache.Default.Dispose()");
+            Console.WriteLine(MemoryCache.Default.Contains("test").ToString());
+            Console.WriteLine(MemoryCache.Default.Count());
 
             Console.ReadKey();
         }

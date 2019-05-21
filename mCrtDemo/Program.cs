0a3a5afef808f00afdd1c174d7c0624ef5daf1e2
Milkey Tan <819774989@qq.com>
2019-05-21 16:34:15 +0800

    crt

---
+++ mCrtDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace mCrt
+{
+    static class Program
+    {
+        static void Main()
+        {
+            X509Certificate2 certificate2 = new X509Certificate2("dst.crt");
+            Console.WriteLine(certificate2.IssuerName.Name);
+            Console.WriteLine(certificate2.SerialNumber);
+            Console.WriteLine(certificate2.Subject);
+            Console.WriteLine(certificate2.SignatureAlgorithm.FriendlyName);
+            Console.WriteLine(certificate2.Version);
+            Console.WriteLine(certificate2.Thumbprint);
+            Console.WriteLine(certificate2.Handle);
+            Console.WriteLine(certificate2.SubjectName.Name);
+            Console.ReadKey();
+        }
+    }
+}

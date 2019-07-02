3f735d4f60d2efde9b804630ec0aa67fc4158c0b
Milkey Tan <819774989@qq.com>
2019-07-02 21:38:36 +0800

    sth

--- mBiliXTagAz.py
+++ mBiliXTagAz.py
@@ -2,24 +2,27 @@ import json,time,openpyxl
 import requests
 from openpyxl import Workbook
 
-tid = 1
+tagId = 1
 page = 1
 
-req = requests.get('https://api.bilibili.com/x/tag/detail?pn={}&ps=40&tag_id={}'.format(page,tid))
+req = requests.get('https://api.bilibili.com/x/tag/detail?pn={}&ps=40&tag_id={}'.format(page,tagId))
 
 jStr = str(req.content,'utf-8')
 jObj = json.loads(jStr)
 resObj = jObj['data']['news']['archives']
+tagName = jObj['data']['info']['tag_name']
 
 wBook = Workbook()
 wSheet = wBook.active
+wSheet.append(['AV号', '标题', '简介', 'up主', '上传时间', '视频时长', ' ', '播放', '弹幕', '回复', '收藏', '硬币' ,' ', '动态标签'])
 
 for r in resObj:
-    avNum = r["aid"]
+    avNum = 'av' + str(r["aid"])
     title = r["title"]
     desc = r["desc"]
     upTime = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(r["ctime"]))
     upUser = r['owner']['name']
+    duration = time.strftime("%H:%M:%S", time.gmtime(float(r["duration"])))
     
     statObj = r['stat']
     view = statObj['view']
@@ -29,7 +32,7 @@ for r in resObj:
     coin = statObj['coin']
 
     tag = r['dynamic']
-    wSheet.append([avNum, title, desc, upUser, upTime, ' ', view, danmaku, reply, favorite, coin ,' ', tag])
+    wSheet.append([avNum, title, desc, upUser, upTime, duration, ' ', view, danmaku, reply, favorite, coin ,' ', tag])
 
-wBook.save("[]-[].xlsx")
+wBook.save("{}-{}-{}.xlsx".format(tagName,str(time.strftime("%Y-%m-%d %H-%M-%S", time.localtime())),page))
     

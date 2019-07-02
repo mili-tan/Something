47b5dc616800b7f6a56b7c959aabef90d25079ac
Milkey Tan <819774989@qq.com>
2019-07-02 21:02:54 +0800

    s2p

--- s2p-iptables.py
+++ s2p-iptables.py
@@ -21,41 +21,53 @@ def index():
 @app.route('/s2p/add')
 def add():
     ip = request.remote_addr
-    if os.geteuid() != 0:
-        os.popen(addcmd.format(ip))
+    if getIpExist(ip) == 1:
+        if os.geteuid() == 0:
+            os.popen(addcmd.format(ip))
+        else:
+            os.popen('sudo ' + addcmd.format(ip,eth))
+        return jsonify({'status':"success",'info':"Success! Now ip {} is added to the rules.".format(ip)})
     else:
-        os.popen('sudo ' + addcmd.format(ip,eth))
-    return "Success! Now your ip " + ip + " is added to the rules."
+        return jsonify({'status':"repeat",'info':"Repeat! Your ip {} already exists in the rules.".format(ip)})
 
 @app.route('/s2p/add/<ip>')
 def addIp(ip):
-    if os.geteuid() != 0:
-        os.popen(addcmd.format(ip))
+    if getIpExist(ip) == 1:
+        if os.geteuid() == 0:
+            os.popen(addcmd.format(ip))
+        else:
+            os.popen('sudo ' + addcmd.format(ip,eth))
+        return jsonify({'status':"success",'info':"Success! Now ip {} is added to the rules.".format(ip)})
     else:
-        os.popen('sudo ' + addcmd.format(ip,eth))
-    return "Success! Now ip " + ip + " is added to the rules."
+        return jsonify({'status':"repeat",'info':"Repeat! Your ip {} already exists in the rules.".format(ip)})
 
 @app.route('/s2p/del/<ip>')
 def delIp(ip):
-    if os.geteuid() != 0:
-        os.popen(delcmd.format(ip))
+    if getIpExist(ip) == 0:
+        if os.geteuid() == 0:
+            os.popen(delcmd.format(ip))
+        else:
+            os.popen('sudo ' + delcmd.format(ip,eth))
+        return jsonify({'status':"success",'info':"Success! Now ip {} is removed from the rules.".format(ip)})
     else:
-        os.popen('sudo ' + delcmd.format(ip,eth))
-    return "Success! Now ip " + ip + " is removed from the rules."
+        return jsonify({'status':"notexists",'info':"Error! Your ip {} not exists in the rules.".format(ip)})
 
 @app.route('/s2p/get/<ip>')
+def getIp(ip):
+    return jsonify({'value':getIpExist(ip)})
+
 def getIpExist(ip):
-    code = 010
-    if os.geteuid() != 0:
+    code = 1
+    if os.geteuid() == 0:
         #code = os.popen(checkcmd.format(ip,eth) + '; echo $?').read()
         code = os.system(checkcmd.format(ip,eth))/255
     else:
         #code = os.popen('sudo ' + checkcmd.format(ip,eth) + '; echo $?').read()
         code = os.system('sudo ' + checkcmd.format(ip,eth))/255
-    return str(code)
+    return code
 
 if __name__ == '__main__':
-    print('mS2p')
+    print('S2P')
     HOST = os.environ.get('SERVER_HOST', '0.0.0.0')
     try:
         PORT = int(os.environ.get('SERVER_PORT', '5555'))

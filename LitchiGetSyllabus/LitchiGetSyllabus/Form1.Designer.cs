1a7bf54a1dc268e551494c9ee691e25d48c866c9
Milkey Tan <819774989@qq.com>
2019-10-20 20:36:55 +0800

    LitchiGetSyllabus

---
+++ LitchiGetSyllabus/LitchiGetSyllabus/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace LitchiGetSyllabus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.semester = new System.Windows.Forms.NumericUpDown();
+            this.week = new System.Windows.Forms.NumericUpDown();
+            this.yr2 = new System.Windows.Forms.NumericUpDown();
+            this.yr1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.MakeTransparent = new System.Windows.Forms.CheckBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.semester)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.week)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yr2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yr1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Enabled = false;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.button1.Location = new System.Drawing.Point(0, 520);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(982, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "开始采集";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.label3, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.semester, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.week, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.yr2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(129, -3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(248, 112);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // semester
+            // 
+            this.semester.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.semester.Location = new System.Drawing.Point(3, 42);
+            this.semester.Maximum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.semester.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.semester.Name = "semester";
+            this.semester.Size = new System.Drawing.Size(118, 27);
+            this.semester.TabIndex = 0;
+            this.semester.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // week
+            // 
+            this.week.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.week.Location = new System.Drawing.Point(3, 79);
+            this.week.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.week.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.week.Name = "week";
+            this.week.Size = new System.Drawing.Size(118, 27);
+            this.week.TabIndex = 1;
+            this.week.Value = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            // 
+            // yr2
+            // 
+            this.yr2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.yr2.Location = new System.Drawing.Point(3, 5);
+            this.yr2.Maximum = new decimal(new int[] {
+            3000,
+            0,
+            0,
+            0});
+            this.yr2.Name = "yr2";
+            this.yr2.Size = new System.Drawing.Size(118, 27);
+            this.yr2.TabIndex = 2;
+            this.yr2.Value = new decimal(new int[] {
+            2020,
+            0,
+            0,
+            0});
+            // 
+            // yr1
+            // 
+            this.yr1.Location = new System.Drawing.Point(3, 3);
+            this.yr1.Maximum = new decimal(new int[] {
+            3000,
+            0,
+            0,
+            0});
+            this.yr1.Name = "yr1";
+            this.yr1.Size = new System.Drawing.Size(123, 27);
+            this.yr1.TabIndex = 3;
+            this.yr1.Value = new decimal(new int[] {
+            2019,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(127, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "学年";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Location = new System.Drawing.Point(127, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "学期";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Location = new System.Drawing.Point(127, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "周";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.yr1);
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.panel1.Location = new System.Drawing.Point(45, 44);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(377, 107);
+            this.panel1.TabIndex = 4;
+            // 
+            // MakeTransparent
+            // 
+            this.MakeTransparent.AutoSize = true;
+            this.MakeTransparent.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.MakeTransparent.Location = new System.Drawing.Point(45, 158);
+            this.MakeTransparent.Name = "MakeTransparent";
+            this.MakeTransparent.Size = new System.Drawing.Size(136, 24);
+            this.MakeTransparent.TabIndex = 5;
+            this.MakeTransparent.Text = "透明化图片背景";
+            this.MakeTransparent.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(45, 188);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(377, 124);
+            this.listBox1.TabIndex = 6;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(45, 319);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(377, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 553);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.MakeTransparent);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.semester)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.week)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yr2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yr1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown semester;
+        private System.Windows.Forms.NumericUpDown week;
+        private System.Windows.Forms.NumericUpDown yr2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown yr1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox MakeTransparent;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ LitchiGetSyllabus/LitchiGetSyllabus/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LitchiGetSyllabus
+{
+    public partial class Form1 : Form
+    {
+        public static WebBrowser MWebBrowser = new WebBrowser();
+        public static string PHPSESSID;
+        public static string PHPSESSID_NS_Sig;
+        public static string IP = IPAddress.Parse(File.ReadAllText("ip.text")).ToString();
+
+        private const int INTERNET_COOKIE_HTTPONLY = 0x00002000;
+
+        [DllImport("wininet.dll", SetLastError = true)]
+        private static extern bool InternetGetCookieEx(
+            string url,
+            string cookieName,
+            StringBuilder cookieData,
+            ref int size,
+            int flags,
+            IntPtr pReserved);
+
+        public static string GetCookie(string url)
+        {
+            int size = 512;
+            StringBuilder strBuilder = new StringBuilder(size);
+            if (InternetGetCookieEx(url, null, strBuilder, ref size, INTERNET_COOKIE_HTTPONLY, IntPtr.Zero))
+                return strBuilder.ToString();
+            if (size < 0)
+                return null;
+            strBuilder = new StringBuilder(size);
+            return !InternetGetCookieEx(url, null, strBuilder, ref size, INTERNET_COOKIE_HTTPONLY, IntPtr.Zero) ? null : strBuilder.ToString();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            MWebBrowser.Url = new Uri($"http://{IP}/studentportal.php/");
+            MWebBrowser.DocumentCompleted += (sender, args) =>
+            {
+                if (MWebBrowser.Url.ToString() == $"http://{IP}/studentportal.php/Main")
+                {
+                    var c1 = GetCookie(MWebBrowser.Url.ToString()).Split(';');
+                    PHPSESSID = c1[0].Split('=')[1];
+                    PHPSESSID_NS_Sig = c1[1].Split('=')[1];
+                    MWebBrowser.Stop();
+                    MWebBrowser.Hide();
+                    button1.Enabled = true;
+                }
+            };
+            Controls.Add(MWebBrowser);
+            MWebBrowser.Dock = DockStyle.Fill;
+            MWebBrowser.BringToFront();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BackgroundWorker bgWorker = new BackgroundWorker();
+            bgWorker.WorkerReportsProgress = true;
+            bgWorker.ProgressChanged += (o, args) => { progressBar1.Value = args.ProgressPercentage; };
+            var index = 1;
+            bgWorker.DoWork += (o, args) =>
+            {
+                foreach (var itemLine in File.ReadAllLines("./list.text"))
+                {
+                    var item = itemLine.Split(',');
+                    var img = WebSnapshotsHelper.GetWebSiteThumbnail($"http://{IP}/studentportal.php/" +
+                                                                     $"Jxxx/xskbxx/optype/2/xn/{yr1.Text}-{yr2.Text}/xq/{semester.Text}/dqz/{week.Text}/" +
+                                                                     $"sybmdmstr/{item[0]}/bjmc/{item[1]}"
+                        , 1200, 600, 1200, 600);
+
+                    Bitmap bitmap = new Bitmap(img);
+                    if (MakeTransparent.Checked) bitmap.MakeTransparent(Color.White);
+                    bitmap.Save($"{item[1]}.jpg");
+                    bgWorker.ReportProgress(index++);
+                    Thread.Sleep(1000);
+                }
+
+                MessageBox.Show(@"done!");
+            };
+            bgWorker.RunWorkerAsync();
+        }
+
+        public class WebSnapshotsHelper
+        {
+            [DllImport("wininet.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            public static extern bool InternetSetCookie(string lpszUrlName, string lbszCookieName, string lpszCookieData);
+
+            Bitmap mBitmap;
+            readonly string mUrl;
+            int mBrowserWidth, mBrowserHeight, mThumbnailWidth, mThumbnailHeight;
+            public WebSnapshotsHelper(string Url, int BrowserWidth, int BrowserHeight, int ThumbnailWidth, int ThumbnailHeight)
+            {
+                mUrl = Url;
+                mBrowserHeight = BrowserHeight;
+                mBrowserWidth = BrowserWidth;
+                mThumbnailWidth = ThumbnailWidth;
+                mThumbnailHeight = ThumbnailHeight;
+            }
+            public static Bitmap GetWebSiteThumbnail(string Url, int BrowserWidth, int BrowserHeight, int ThumbnailWidth, int ThumbnailHeight)
+            {
+                WebSnapshotsHelper thumbnailGenerator = new WebSnapshotsHelper(Url, BrowserWidth, BrowserHeight, ThumbnailWidth, ThumbnailHeight);
+                return thumbnailGenerator.GenerateWebSiteThumbnailImage();
+            }
+            public Bitmap GenerateWebSiteThumbnailImage()
+            {
+                Thread mThread = new Thread(Generate_WebSiteThumbnailImage);
+                mThread.SetApartmentState(ApartmentState.STA);
+                mThread.Start();
+                mThread.Join();
+                return mBitmap;
+            }
+            private void Generate_WebSiteThumbnailImage()
+            {
+                WebBrowser mWebBrowser = new WebBrowser();
+                InternetSetCookie(mUrl, "PHPSESSID", PHPSESSID);
+                InternetSetCookie(mUrl, "PHPSESSID_NS_Sig", PHPSESSID_NS_Sig);
+                mWebBrowser.ScrollBarsEnabled = false;
+                mWebBrowser.Navigate(mUrl);
+                mWebBrowser.DocumentCompleted += WebBrowser_DocumentCompleted;
+                while (mWebBrowser.ReadyState != WebBrowserReadyState.Complete)
+                    Application.DoEvents();
+                mWebBrowser.Dispose();
+            }
+            private void WebBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+            {
+                WebBrowser mWebBrowser = (WebBrowser)sender;
+                mWebBrowser.ClientSize = new Size(mBrowserWidth, mBrowserHeight);
+                mWebBrowser.ScrollBarsEnabled = false;
+                mBitmap = new Bitmap(mWebBrowser.Bounds.Width, mWebBrowser.Bounds.Height);
+                mWebBrowser.BringToFront();
+                mWebBrowser.DrawToBitmap(mBitmap, mWebBrowser.Bounds);
+                mBitmap = (Bitmap)mBitmap.GetThumbnailImage(mThumbnailWidth, mThumbnailHeight, null, IntPtr.Zero);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var listLines = File.ReadAllLines("./list.text");
+            listBox1.Items.AddRange(listLines);
+            progressBar1.Maximum = listLines.Length;
+        }
+    }
+}

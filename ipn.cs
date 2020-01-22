3e5de70655f82a43687fbd7c5da987ce262d2e39
Milkey Tan <819774989@qq.com>
2020-01-22 19:44:32 +0800

    ipn

---
+++ ipn.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using LukeSkywalker.IPNetwork;
+
+namespace ipn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPNetwork ipn = IPNetwork.Parse("192.168.168.100/24");
+            Console.WriteLine(ipn);
+            IPNetwork ipn2 = IPNetwork.Parse("192.168.168.100");
+            Console.WriteLine(ipn2);
+            Console.ReadKey();
+        }
+    }
+}

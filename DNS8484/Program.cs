f9c7f22a11182ef06ab4d56113f5dad8c37fc0e1
Milkey Tan <819774989@qq.com>
2020-02-06 16:53:08 +0800

    update 8484

--- DNS8484/Program.cs
+++ DNS8484/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Net;
+using System.Reflection;
+using System.Runtime.InteropServices.ComTypes;
+using ARSoft.Tools.Net;
 using ARSoft.Tools.Net.Dns;
 using OhMyDnsPackage;
 
@@ -10,11 +13,23 @@ namespace DNS8484
 
         static void Main()
         {
-            var dnsBase64String = Convert.ToBase64String(MyDnsSend.GetQuestionData("dnsp1.mili.one", RecordType.Aaaa)).TrimEnd('=')
+            var dnsQMsg = new DnsMessage();
+            dnsQMsg.Questions.Add(new DnsQuestion(DomainName.Parse("google.com"), RecordType.A, RecordClass.Any));
+            //dnsQMsg.Encode(false, out var bytes);
+
+            var args = new object[] {false, null};
+            var mms = dnsQMsg.GetType().GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+            foreach (var mm in mms)
+                if (mm.ToString() == "Int32 Encode(Boolean, Byte[] ByRef)")
+                    mm.Invoke(dnsQMsg, args);
+
+            Console.WriteLine(args.Length);
+            //MyDnsSend.GetQuestionData("dnsp1.mili.one", RecordType.Aaaa)
+            var dnsBase64String = Convert.ToBase64String((byte[])args[1]).TrimEnd('=')
                 .Replace('+', '-').Replace('/', '_');
             Console.WriteLine(dnsBase64String);
             var dnsMsg =
-                DnsMessage.Parse(new WebClient().DownloadData("https://dns.rubyfish.cn/dns-query?dns=" + dnsBase64String));
+                DnsMessage.Parse(new WebClient().DownloadData("https://223.5.5.5/dns-query?dns=" + dnsBase64String));
             foreach (var item in dnsMsg.AnswerRecords)
                 Console.WriteLine(item);
 

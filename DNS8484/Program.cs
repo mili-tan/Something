6ae23dc90cf3fa14b0af735ef69c5d930811a206
Milkey Tan <819774989@qq.com>
2020-02-06 19:15:20 +0800

    try fix

--- DNS8484/Program.cs
+++ DNS8484/Program.cs
@@ -14,7 +14,7 @@ namespace DNS8484
         static void Main()
         {
             var dnsQMsg = new DnsMessage();
-            dnsQMsg.Questions.Add(new DnsQuestion(DomainName.Parse("pixiv.net"), RecordType.A, RecordClass.Any));
+            dnsQMsg.Questions.Add(new DnsQuestion(DomainName.Parse("pixiv.net"), RecordType.A, RecordClass.INet));
             dnsQMsg.IsEDnsEnabled = true;
             dnsQMsg.EDnsOptions.Options.Add(new ClientSubnetOption(24, IPAddress.Parse("0.0.0.0")));
             //dnsQMsg.Encode(false, out var bytes);
@@ -25,13 +25,18 @@ namespace DNS8484
                 if (mm.ToString() == "Int32 Encode(Boolean, Byte[] ByRef)")
                     mm.Invoke(dnsQMsg, args);
 
-            Console.WriteLine(args.Length);
-            //MyDnsSend.GetQuestionData("dnsp1.mili.one", RecordType.Aaaa)
+            if ((args[1] as byte[])[2] == 0) (args[1] as byte[])[2] = 1;
+
+            //var ibyte = MyDnsSend.GetQuestionData(DomainName.Parse("baidu.com").ToString().TrimEnd('.'), RecordType.A);
+            //Console.WriteLine(ibyte);
+
             var dnsBase64String = Convert.ToBase64String((byte[])args[1]).TrimEnd('=')
                 .Replace('+', '-').Replace('/', '_');
             Console.WriteLine(dnsBase64String);
             var dnsMsg =
-                DnsMessage.Parse(new WebClient().DownloadData("https://223.5.5.5/dns-query?dns=" + dnsBase64String));
+                DnsMessage.Parse(new WebClient().DownloadData("https://doh1.blahdns.com/dns-query?dns=" + dnsBase64String));
+            
+            Console.WriteLine(dnsMsg.ReturnCode);
             foreach (var item in dnsMsg.AnswerRecords)
                 Console.WriteLine(item);
 

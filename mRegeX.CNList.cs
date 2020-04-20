9861b6732911edc4e904d9971b37eccc997a0c41
Milkey Tan <819774989@qq.com>
2020-04-20 21:45:25 +0800

    sth

---
+++ mRegeX.CNList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using ARSoft.Tools.Net;
+
+namespace mRegeX.ChinaName
+{
+    class Program
+    {
+        private static List<DomainName> chinaList = File.ReadAllLines("china_whitelist.list")
+            .ToList().ConvertAll(DomainName.Parse);
+        static void Main(string[] args)
+        {
+            //File.WriteAllLines("china_whitelist.list", list.ConvertAll(name => name.ToString().TrimEnd('.')));
+
+            while (true)
+            {
+                var name = DomainName.Parse(Console.ReadLine());
+                var stopWatch = new Stopwatch();
+                stopWatch.Start();
+                //foreach (var itemName in list)
+                //{
+                //    if (!name.IsEqualOrSubDomainOf(itemName)) continue;
+                //    Console.WriteLine(itemName);
+                //    break;
+                //}
+                Console.WriteLine(IsChinaName(name));
+                stopWatch.Stop();
+                Console.WriteLine("done:" + stopWatch.ElapsedTicks);
+            }
+        }
+
+        public static bool IsChinaName(DomainName name)
+        {
+            return chinaList.Any(name.IsEqualOrSubDomainOf);
+        }
+    }
+}

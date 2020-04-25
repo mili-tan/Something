bfc98a9793740113818dccf77d6ba2569e13977b
Milkey Tan <819774989@qq.com>
2020-04-25 21:43:46 +0800

    ETCD

---
+++ Playground/ETCD.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using dotnet_etcd;
+using Etcdserverpb;
+using Google.Protobuf;
+
+namespace ETCD_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var etcdClient = new EtcdClient("http://localhost:2379");
+            etcdClient.DeleteRange("/foo");
+            //Console.WriteLine(etcdClient.GetVal("/foo"));
+            //Console.WriteLine(etcdClient.Get("foo").Kvs.FirstOrDefault().Value.ToStringUtf8());
+            etcdClient.Put($"/foo/{Guid.NewGuid()}", Guid.NewGuid().ToString());
+            etcdClient.Put(new PutRequest
+            {
+                Key = ByteString.CopyFromUtf8("/time"),
+                Value = ByteString.CopyFromUtf8(Guid.NewGuid().ToString()),
+                Lease = etcdClient.LeaseGrant(new LeaseGrantRequest
+                {
+                    TTL = 20
+                }).ID
+            });
+            var response = etcdClient.GetRange("/");
+            foreach (var keyValue in response.Kvs)
+            {
+                Console.WriteLine($"{keyValue.Key.ToStringUtf8()}:{keyValue.Value.ToStringUtf8()}");
+            }
+            Console.ReadKey();
+        }
+    }
+}

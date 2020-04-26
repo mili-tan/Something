16b042cc77eb287fb4a1163c0a96f360d974ba2d
Milkey Tan <819774989@qq.com>
2020-04-26 21:26:43 +0800

    sth

---
+++ Playground/mRocksDB.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using RocksDbSharp;
+
+namespace mRocksDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbOptions = new DbOptions().SetCreateIfMissing().SetCreateMissingColumnFamilies();
+            var columnFamilies = new ColumnFamilies
+            {
+                {"default", new ColumnFamilyOptions().OptimizeForPointLookup(256)},
+                {
+                    "user", new ColumnFamilyOptions()
+                        .SetMemtableHugePageSize(2 * 1024 * 1024)
+                        .SetPrefixExtractor(SliceTransform.CreateFixedPrefix(5))
+                        .SetBlockBasedTableFactory(
+                            new BlockBasedTableOptions().SetFilterPolicy(BloomFilterPolicy.Create(10, false))
+                            .SetWholeKeyFiltering(false))
+                }
+            };
+            var db = RocksDb.Open(dbOptions, "my.db", columnFamilies);
+            var testFamily = db.GetColumnFamily("user");
+            
+            db.Put("hello:hi","hi");
+            for (var i = 0; i < 5; i++) db.Put($"user:{Guid.NewGuid()}", Guid.NewGuid().ToString(), testFamily);
+            using (var it = db.NewIterator(testFamily))
+            {
+                it.Seek(Encoding.UTF8.GetBytes("user:"));
+                while (it.Valid())
+                {
+                    Console.WriteLine(it.StringKey());
+                    it.Next();
+                }
+            }
+
+            Console.WriteLine("Done!" + db.Get("hello:hi"));
+        }
+    }
+}

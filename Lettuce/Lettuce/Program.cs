bf24d2a0804d3b9baaa2d2cd1c430b6be12a86fb
Milkey Tan <819774989@qq.com>
2020-07-01 20:07:24 +0800

    sth

---
+++ Lettuce/Lettuce/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.IO;
+using System.Net;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
+using LettuceEncrypt;
+using Microsoft.Extensions.Logging;
+
+namespace Lettuce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .ConfigureLogging(configureLogging => { configureLogging.AddConsole(); })
+                .UseContentRoot(AppDomain.CurrentDomain.SetupInformation.ApplicationBase)
+                .ConfigureServices(services =>
+                {
+                    services.AddRouting();
+                    services.AddLettuceEncrypt(configure =>
+                    {
+                        configure.AcceptTermsOfService = true;
+                        configure.DomainNames = new[] {"test.mili.one"};
+                        configure.EmailAddress = "me@milione.cc";
+                    }).PersistDataToDirectory(new DirectoryInfo("/LettuceEncrypt"), null);
+                })
+                .UseKestrel(k =>
+                {
+                    //var appServices = k.ApplicationServices;
+                    //k.ConfigureHttpsDefaults(h =>
+                    //{
+                    //    h.ClientCertificateMode = ClientCertificateMode.RequireCertificate;
+                    //    h.UseLettuceEncrypt(appServices);
+                    //});
+                })
+                .ConfigureKestrel(options =>
+                {
+                    options.Limits.MaxRequestBodySize = 1024;
+                    options.Listen(new IPEndPoint(IPAddress.Any, 80),
+                        listenOptions => { listenOptions.Protocols = HttpProtocols.Http1AndHttp2; });
+                })
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ Lettuce/Lettuce/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Lettuce
+{
+    class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+            app.UseRouting().UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    context.Response.ContentType = "text/html";
+                    await context.Response.WriteAsync("hello ");
+                });
+            });
+        }
+    }
+}

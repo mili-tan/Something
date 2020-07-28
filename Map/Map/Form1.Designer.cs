9201a0ef8aea7246d70fe6c00028d22efc453d96
Milkey Tan <819774989@qq.com>
2020-07-28 21:51:06 +0800

    sth

--- Map/Map/Form1.Designer.cs
+++ Map/Map/Form1.Designer.cs
@@ -28,21 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.gMapC = new GMap.NET.WindowsForms.GMapControl();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // gMapC
             // 
-            this.gMapC.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gMapC.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.gMapC.Bearing = 0F;
             this.gMapC.CanDragMap = true;
             this.gMapC.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapC.Font = new System.Drawing.Font("微软雅黑", 9F);
             this.gMapC.GrayScaleMode = false;
             this.gMapC.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
             this.gMapC.LevelsKeepInMemory = 5;
-            this.gMapC.Location = new System.Drawing.Point(13, 13);
+            this.gMapC.Location = new System.Drawing.Point(0, 0);
             this.gMapC.MarkersEnabled = true;
             this.gMapC.MaxZoom = 19;
             this.gMapC.MinZoom = 2;
@@ -56,19 +61,55 @@
             this.gMapC.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
             this.gMapC.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
             this.gMapC.ShowTileGridLines = false;
-            this.gMapC.Size = new System.Drawing.Size(981, 528);
+            this.gMapC.Size = new System.Drawing.Size(717, 553);
             this.gMapC.TabIndex = 0;
             this.gMapC.Zoom = 5D;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.listBox);
+            this.panel1.Location = new System.Drawing.Point(717, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(289, 553);
+            this.panel1.TabIndex = 1;
+            // 
+            // listBox
+            // 
+            this.listBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox.Font = new System.Drawing.Font("微软雅黑", 10F);
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 23;
+            this.listBox.Items.AddRange(new object[] {
+            "test,test,test"});
+            this.listBox.Location = new System.Drawing.Point(0, 0);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(289, 553);
+            this.listBox.TabIndex = 0;
+            this.listBox.SelectedIndexChanged += new System.EventHandler(this.listBox_SelectedIndexChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1006, 553);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.gMapC);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -76,6 +117,9 @@
         #endregion
 
         private GMap.NET.WindowsForms.GMapControl gMapC;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Map/Map/Form1.cs
+++ Map/Map/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using GMap.NET;
 using GMap.NET.MapProviders;
@@ -25,14 +26,46 @@ namespace Map
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            gMapC.Zoom = 13;
-            GMapOverlay markersOverlay = new GMapOverlay("markers");
-            markersOverlay.Markers.Add(new GMarkerGoogle(new PointLatLng(26.03298775, 119.204552503912),
-                GMarkerGoogleType.red_dot){ToolTipText = "FJNU"});
-            markersOverlay.Markers.Add(new GMarkerGoogle(new PointLatLng(26.0613597, 119.193340735152),
-                    GMarkerGoogleType.red_dot) { ToolTipText = "FZU" });
+            listBox.Items.Clear();
+            listBox.Items.Add("26.03298775,119.204552503912,福建师范大学");
+            listBox.Items.Add("26.0613597,119.193340735152,福州大学");
+            listBox.Items.Add("26.07798425,119.268145831242,福州大学至诚学院");
+            listBox.Items.Add("26.0888446,119.367328358362,福建工程学院");
+            listBox.Items.Add("26.086128,119.232176390959,福建农林大学金山学院");
+        }
+
+        private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var itemStrs = listBox.SelectedItem.ToString().Split(',');
+            var latLng = new PointLatLng(Convert.ToDouble(itemStrs[0].Trim()), Convert.ToDouble(itemStrs[1].Trim()));
+            //var markersOverlay = new GMapOverlay("imarker");
+            //markersOverlay.Markers.Add(new GMarkerGoogle(latLng, GMarkerGoogleType.red_dot)
+            //    {ToolTipText = itemStrs[2].Trim()});
+            //gMapC.Overlays.Add(markersOverlay);
+            gMapC.Position = latLng;
+            gMapC.Zoom = 15;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            gMapC.Zoom -= 1;
+            gMapC.Overlays.Clear();
+            var markersOverlay = new GMapOverlay("markers");
+            Task.Run(() =>
+            {
+                foreach (var item in listBox.Items)
+                {
+                    var itemStrs = item.ToString().Split(',');
+                    markersOverlay.Markers.Add(
+                        new GMarkerGoogle(
+                                new PointLatLng(Convert.ToDouble(itemStrs[0].Trim()), Convert.ToDouble(itemStrs[1].Trim())),
+                                GMarkerGoogleType.red_dot)
+                            { ToolTipText = itemStrs[2].Trim() });
+                }
+            });
             gMapC.Overlays.Add(markersOverlay);
-            gMapC.Zoom = 12;
+            gMapC.Zoom += 1;
+            gMapC.Refresh();
         }
     }
 }

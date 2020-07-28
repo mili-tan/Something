d0c30521791cffa9d22b3f2eab7720546f88d546
Milkey Tan <819774989@qq.com>
2020-07-28 20:42:32 +0800

    map

---
+++ Map/Map/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Map
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gMapC = new GMap.NET.WindowsForms.GMapControl();
+            this.SuspendLayout();
+            // 
+            // gMapC
+            // 
+            this.gMapC.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gMapC.Bearing = 0F;
+            this.gMapC.CanDragMap = true;
+            this.gMapC.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapC.GrayScaleMode = false;
+            this.gMapC.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapC.LevelsKeepInMemory = 5;
+            this.gMapC.Location = new System.Drawing.Point(13, 13);
+            this.gMapC.MarkersEnabled = true;
+            this.gMapC.MaxZoom = 19;
+            this.gMapC.MinZoom = 2;
+            this.gMapC.MouseWheelZoomEnabled = true;
+            this.gMapC.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapC.Name = "gMapC";
+            this.gMapC.NegativeMode = false;
+            this.gMapC.PolygonsEnabled = true;
+            this.gMapC.RetryLoadTile = 0;
+            this.gMapC.RoutesEnabled = true;
+            this.gMapC.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapC.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapC.ShowTileGridLines = false;
+            this.gMapC.Size = new System.Drawing.Size(981, 528);
+            this.gMapC.TabIndex = 0;
+            this.gMapC.Zoom = 5D;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 553);
+            this.Controls.Add(this.gMapC);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gMapC;
+    }
+}
+
---
+++ Map/Map/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+
+namespace Map
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            GMaps.Instance.Mode = AccessMode.ServerAndCache;
+            gMapC.MapProvider = GMapProviders.GoogleChinaMap;//设置底图为谷歌中国地图
+            //gMapC.SetPositionByKeywords("Fuzhou, China");
+            gMapC.DragButton = MouseButtons.Left;
+            gMapC.MaxZoom = 19;//最大显示级别
+            gMapC.MinZoom = 3;//最小级别
+            gMapC.Zoom = 12;//初始化时的缩放级别（参考瓦片地图的级别划分）
+            gMapC.Position = new PointLatLng(26.10, 119.20);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gMapC.Zoom = 13;
+            GMapOverlay markersOverlay = new GMapOverlay("markers");
+            markersOverlay.Markers.Add(new GMarkerGoogle(new PointLatLng(26.03298775, 119.204552503912),
+                GMarkerGoogleType.red_dot){ToolTipText = "FJNU"});
+            markersOverlay.Markers.Add(new GMarkerGoogle(new PointLatLng(26.0613597, 119.193340735152),
+                    GMarkerGoogleType.red_dot) { ToolTipText = "FZU" });
+            gMapC.Overlays.Add(markersOverlay);
+            gMapC.Zoom = 12;
+        }
+    }
+}

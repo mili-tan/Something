a228ad4cbfe913c28ec47858a72dd1c6d8a1b22a
Milkey Tan <819774989@qq.com>
2020-12-02 23:37:04 +0800

    OnePage

---
+++ OnePage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OnePageSev
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var file = File.ReadAllText("index.html");
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(AppDomain.CurrentDomain.SetupInformation.ApplicationBase)
+                .ConfigureServices(services => services.AddRouting())
+                .ConfigureKestrel(options =>
+                {
+                    options.Limits.MaxRequestBodySize = 1024;
+                    options.Listen(new IPEndPoint(IPAddress.Loopback, 2800),
+                        listenOptions => listenOptions.Protocols = HttpProtocols.Http1AndHttp2);
+                })
+                .Configure(app =>
+                {
+                    app.UseStaticFiles();
+                    app.UseDefaultFiles();
+                    app.UseRouting().UseEndpoints(endpoints =>
+                    {
+                        endpoints.Map("/", async context =>
+                        {
+                            context.Response.Headers.Add("X-Powered-By", "DreamLayer.OnePage");
+                            context.Response.ContentType = "text/html";
+                            await context.Response.WriteAsync(file);
+                        });
+                        endpoints.Map("/{any}", async context =>
+                        {
+                            context.Response.Headers.Add("X-Powered-By", "DreamLayer.OnePage");
+                            context.Response.ContentType = "text/html";
+                            await context.Response.WriteAsync(file);
+                        });
+                        endpoints.Map("/{any}/{sth}", async context =>
+                        {
+                            context.Response.Headers.Add("X-Powered-By", "DreamLayer.OnePage");
+                            context.Response.ContentType = "text/html";
+                            await context.Response.WriteAsync(file);
+                        });
+                    });
+                })
+                .Build();
+            host.Run();
+        }
+    }
+}

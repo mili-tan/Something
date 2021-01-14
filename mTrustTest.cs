4c76ec2ffdfa4062c3b6befaceb5bc14c011c749
Milkey Tan <819774989@qq.com>
2021-01-14 20:25:08 +0800

    sth

---
+++ mTrustTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using PgpCore;
+
+namespace mTrustME
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pgp = new PGP();
+            new WebClient().DownloadFile("https://github.com/mili-tan.gpg", "mili-tan.asc");
+            var verify = pgp.VerifyFile("linux-arm64.zip.gpg", "mili-tan.asc");
+            Console.WriteLine(verify);
+        }
+    }
+}

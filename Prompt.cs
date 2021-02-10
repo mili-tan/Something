111d4cd05b908ec9c9324d95016b8b4932f9765a
Milkey Tan <819774989@qq.com>
2021-02-10 19:49:33 +0800

    Prompt

---
+++ Prompt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Sharprompt;
+
+namespace SharpromptTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var city = Prompt.Select("Select your city", new[] { "Seattle", "London", "Tokyo" });
+            Console.WriteLine($"Hello, {city}!");
+            var answer = Prompt.Confirm("Are you ready?");
+            Console.WriteLine($"Your answer is {answer}");
+        }
+    }
+}

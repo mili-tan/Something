8880cf70dfa15b059e33f8fdd557ae7a791fd57b
Milkey Tan <819774989@qq.com>
2021-02-24 20:28:14 +0800

    jwt

---
+++ jwt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using JWT.Algorithms;
+using JWT.Builder;
+using Newtonsoft.Json.Linq;
+
+namespace JWToken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var secret = Guid.NewGuid();
+            var token = new JwtBuilder()
+                .WithAlgorithm(new HMACSHA256Algorithm())
+                .WithSecret(secret.ToString())
+                .ExpirationTime(DateTimeOffset.UtcNow.AddHours(24).ToUnixTimeSeconds())
+                .IssuedAt(DateTime.Now)
+                .AddClaim("token", secret)
+                .Encode();
+            Console.WriteLine(token);
+
+            var json = new JwtBuilder()
+                .WithAlgorithm(new HMACSHA256Algorithm())
+                .WithSecret(secret.ToString())
+                .MustVerifySignature()
+                .Decode<JObject>(token);
+            Console.WriteLine(json["token"].ToString());
+        }
+    }
+}

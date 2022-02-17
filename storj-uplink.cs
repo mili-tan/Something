5d2b42e10a0e2c720cea4263a38401938b613738
Milkey Tan <me@milione.cc>
2022-02-17 21:43:44 +0800

    Add files via upload

---
+++ storj-uplink.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using uplink.NET.Models;
+using uplink.NET.Services;
+
+namespace UpLinkTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var access = new Access("grant-here");
+            var bucketService = new BucketService(access);
+            var buckets = bucketService.ListBucketsAsync(new ListBucketsOptions()).Result;
+            foreach (var item in buckets.Items)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            var bucket = bucketService.GetBucketAsync("test").Result;
+
+            var objectService = new ObjectService(access);
+            var bytesToUpload = Encoding.UTF8.GetBytes("Storj is awesome!");
+
+            for (int i = 0; i < 5; i++)
+            {
+                objectService
+                    .UploadObjectAsync(bucket, $"test-time/{Guid.NewGuid()}.txt", new UploadOptions { Expires = DateTime.UtcNow.AddDays(1) },
+                        bytesToUpload, false).Result
+                    .StartUploadAsync().GetAwaiter().GetResult();
+            }
+
+            var listObjectsOptions = new ListObjectsOptions
+            {
+                Prefix = "test/"
+            };
+
+            var objectList = objectService.ListObjectsAsync(bucket, listObjectsOptions).Result;
+
+            foreach (var item in objectList.Items)
+            {
+                Console.WriteLine(item.Key);
+            }
+        }
+    }
+}

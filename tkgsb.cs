0350578907d42fe204908bd6baad30e2954a4e4e
Milkey Tan <me@milione.cc>
2023-08-27 22:26:44 +0800

    Update tkgsb.cs

--- tkgsb.cs
+++ tkgsb.cs
@@ -2,6 +2,10 @@ using System.Runtime.Caching;
 using System.Security.Cryptography;
 using System.Text;
 using System.Xml.Linq;
+using System.IO.Compression;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
 
 namespace TenKGSB
 {
@@ -17,14 +21,16 @@ namespace TenKGSB
 
             app.MapGet("/t", async (HttpContext httpContext, IHttpClientFactory clientFactory) =>
             {
+                httpContext.Response.ContentType = "application/json;charset=UTF-8";
+
                 var name = httpContext.Request.Query["name"];
                 if (MemoryCache.Default.Contains("T:" + name))
-                    return MemoryCache.Default.Get("T:" + name).ToString();
+                    return Decompress((byte[]) MemoryCache.Default.Get("T:" + name));
 
                 var httpClient = clientFactory.CreateClient("T");
                 httpClient.DefaultRequestHeaders.Add("Client-Ip", "192.168.1.100");
                 httpClient.DefaultRequestHeaders.Add("X-Forwarded-For", "192.168.1.100");
-                httpClient.DefaultRequestHeaders.Add("Referer", "https://../");
+                httpClient.DefaultRequestHeaders.Add("Referer", "https://.../");
 
                 var fullUrl = "https://.../index.php?m=check&a=check&url=" + name;
                 var results = await (await httpClient.PostAsync(fullUrl, null)).Content.ReadAsStringAsync();
@@ -35,21 +41,22 @@ namespace TenKGSB
                 {
                     if (!MemoryCache.Default.Contains("T:" + name))
                         MemoryCache.Default.Add(
-                            new CacheItem("T:" + name, json),
+                            new CacheItem("T:" + name, Compress(json)),
                             new CacheItemPolicy {AbsoluteExpiration = DateTimeOffset.Now.AddDays(1)});
                 });
 
-                httpContext.Response.ContentType = "application/json;charset=UTF-8";
                 return json;
             });
 
             app.MapGet("/k", async (HttpContext httpContext, IHttpClientFactory clientFactory) =>
             {
+                httpContext.Response.ContentType = "application/json;charset=UTF-8";
+
                 var name = httpContext.Request.Query["name"];
                 if (MemoryCache.Default.Contains("K:" + name))
-                    return MemoryCache.Default.Get("K:" + name).ToString();
+                    return Decompress((byte[]) MemoryCache.Default.Get("K:" + name));
 
-                var appKey = "k-";
+                var appKey = "";
                 var secret = "";
 
                 var q = Convert.ToBase64String(Encoding.UTF8.GetBytes(name));
@@ -68,11 +75,10 @@ namespace TenKGSB
                 {
                     if (!MemoryCache.Default.Contains("K:" + name))
                         MemoryCache.Default.Add(
-                            new CacheItem("K:" + name, result),
+                            new CacheItem("K:" + name, Compress(result)),
                             new CacheItemPolicy {AbsoluteExpiration = DateTimeOffset.Now.AddDays(1)});
                 });
 
-                httpContext.Response.ContentType = "application/json;charset=UTF-8";
                 return result;
             });
 
@@ -90,6 +96,24 @@ namespace TenKGSB
                 builder.Append(b.ToString("X2"));
 
             return builder.ToString();
+        }
+
+        public static byte[] Compress(string str)
+        {
+            var bytes = Encoding.UTF8.GetBytes(str);
+            using var memoryStream = new MemoryStream();
+            using (var brotliStream = new BrotliStream(memoryStream, CompressionLevel.Optimal))
+                brotliStream.Write(bytes, 0, bytes.Length);
+            return memoryStream.ToArray();
+        }
+
+        public static string Decompress(byte[] bytes)
+        {
+            using var memoryStream = new MemoryStream(bytes);
+            using var outputStream = new MemoryStream();
+            using (var decompressStream = new BrotliStream(memoryStream, CompressionMode.Decompress))
+                decompressStream.CopyTo(outputStream);
+            return Encoding.UTF8.GetString(outputStream.ToArray());
         }
     }
 }
